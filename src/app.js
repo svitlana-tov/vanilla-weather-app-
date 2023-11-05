7b68c212b878a6a45cc253286585a9da88acf67e
svitlana-tov <147521374+svitlana-tov@users.noreply.github.com>
2023-11-05 15:06:03 -0500

    added basic search engine (no API)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
