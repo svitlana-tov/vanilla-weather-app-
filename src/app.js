e82aaa07440f461a3401bf700e57585103401ab3
svitlana-tov <147521374+svitlana-tov@users.noreply.github.com>
2023-11-05 17:30:03 -0500

    injected more weather data

--- src/app.js
+++ src/app.js
@@ -4,7 +4,55 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#weather-app-condition");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let feelsLikeElement = document.querySelector("#feels-like");
+  feelsLikeElement.innerHTML = `${response.data.temperature.feels_like}°C`;
+  let timeElement = document.querySelector("#weather-app-time");
+
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
 }
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let dateD = date.getDate();
+  let year = date.getFullYear();
+  return `${month} ${dateD}, ${day}, ${hours}:${minutes}`;
+}
+
 function searchCity(city) {
   let apiKey = "bdbfc2eac4o25bf371f546te0a81fd3b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
