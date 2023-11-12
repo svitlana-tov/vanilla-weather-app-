61d09c432dcca4c650c0130896bcb475a011406a
svitlana-tov <147521374+svitlana-tov@users.noreply.github.com>
2023-11-12 15:02:57 -0500

    updated the forecast

--- src/app.js
+++ src/app.js
@@ -89,18 +89,19 @@ function displayForecast(response) {
         `
        <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
-        <div >
+        <div>
         <img src="${day.condition.icon_url}"class="weather-forecast-icon" />
         </div>
         <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}º</strong>
-          </div>
-          <div class="weather-forecast-temperature">${Math.round(
+          </span>
+          <span class="weather-forecast-temperature-min">${Math.round(
             day.temperature.minimum
-          )}º</div>
+          )}º</span>
         </div>
-      </div>`;
+      </div>
+     `;
     }
   });
 
